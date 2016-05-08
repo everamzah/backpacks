9ef690e301de7c568d11cbf22c8c14ced317c94a
James Stevenson <everamzah@gmail.com>
2016-05-08 17:17:15 -0400

    Initial commit.

---
+++ init.lua
@@ -0,0 +1,174 @@
+minetest.register_on_joinplayer(function(player)
+	local inv = minetest.create_detached_inventory("backpack_" .. player:get_player_name(), {
+		allow_put = function(inv, listname, index, stack, player)
+			if string.match(stack:get_name(), "backpacks:backpack_") then
+				return 0
+			else
+				return stack:get_count()
+			end
+		end,
+	})
+	inv:set_size("main", 8*4)
+end)
+
+local backpacks = {}
+
+backpacks.form = "size[8,9]" ..
+	default.gui_bg_img ..
+	default.gui_slots ..
+	"list[current_name;main;0,0.3;8,4]" ..
+	"list[current_player;main;0,4.85;8,1]" ..
+	"list[current_player;main;0,6.08;8,3;8]" ..
+	"listring[current_name;main]" ..
+	"listring[current_player;main]"
+
+backpacks.on_construct = function(pos)
+	local meta = minetest.get_meta(pos)
+	meta:set_string("infotext", "Backpack")
+	meta:set_string("formspec", backpacks.form)
+	local inv = meta:get_inventory()
+	inv:set_size("main", 8*4)
+end
+
+backpacks.after_place_node = function(pos, placer, itemstack, pointed_thing)
+	local meta = minetest.get_meta(pos)
+	local stuff = minetest.deserialize(itemstack:get_metadata())
+	if stuff then
+		meta:from_table(stuff)
+	end
+	itemstack:take_item()
+end
+
+backpacks.on_dig = function(pos, node, digger)
+	if minetest.is_protected(pos, digger:get_player_name()) then
+		return false
+	end
+	local meta = minetest.get_meta(pos)
+	local inv = meta:get_inventory()
+
+	local list = {}
+	for i, stack in ipairs(inv:get_list("main")) do
+		if stack:get_name() == "" then
+			list[i] = ""
+		else 
+			list[i] = stack:to_string()
+		end
+	end
+
+	local new_list = {inventory = {main = list},
+			fields = {infotext = "Backpack", formspec = backpacks.form}}
+	local new_list_as_string = minetest.serialize(new_list)
+
+	local new = ItemStack(node)
+	new:set_metadata(new_list_as_string)
+
+	minetest.remove_node(pos)
+
+	local player_inv = digger:get_inventory()
+	if player_inv:room_for_item("main", new) then
+		player_inv:add_item("main", new)
+	else
+		minetest.add_item(pos, new)
+	end
+end
+
+backpacks.allow_metadata_inventory_put = function(pos, listname, index, stack, player)
+	if not string.match(stack:get_name(), "backpacks:backpack_") then
+		return stack:get_count()
+	else
+		return 0
+	end
+end
+
+-- Wool Backpacks
+for _, v in ipairs(dye.dyes) do
+	minetest.register_node("backpacks:backpack_wool_" .. v[1], {
+		description = "Backpack (" .. v[2] .. " Wool)",
+		tiles = {
+			"wool_" .. v[1] .. ".png^backpacks_backpack_wool_topbottom.png", -- Top
+			"wool_" .. v[1] .. ".png^backpacks_backpack_wool_topbottom.png", -- Bottom
+			"wool_" .. v[1] .. ".png^backpacks_backpack_wool_sides.png", -- Right Side
+			"wool_" .. v[1] .. ".png^backpacks_backpack_wool_sides.png", -- Left Side
+			"wool_" .. v[1] .. ".png^backpacks_backpack_wool_back.png", -- Back
+			"wool_" .. v[1] .. ".png^backpacks_backpack_wool_front.png"-- Front
+		},
+		drawtype = "nodebox",
+		paramtype = "light",
+		paramtype2 = "facedir",
+		node_box = {
+			type = "fixed",
+			fixed = {
+				{-0.4375, -0.5, -0.375, 0.4375, 0.5, 0.375},
+				{0.125, -0.375, 0.4375, 0.375, 0.3125, 0.5},
+				{-0.375, -0.375, 0.4375, -0.125, 0.3125, 0.5},
+				{0.125, 0.1875, 0.375, 0.375, 0.375, 0.4375},
+				{-0.375, 0.1875, 0.375, -0.125, 0.375, 0.4375},
+				{0.125, -0.375, 0.375, 0.375, -0.25, 0.4375},
+				{-0.375, -0.375, 0.375, -0.125, -0.25, 0.4375},
+				{-0.3125, -0.375, -0.4375, 0.3125, 0.1875, -0.375},
+				{-0.25, -0.3125, -0.5, 0.25, 0.125, -0.4375},
+			}
+		},
+		groups = {dig_immediate = 3, oddly_diggable_by_hand = 3},
+		stack_max = 1,
+		on_construct = backpacks.on_construct,
+		after_place_node = backpacks.after_place_node,
+		on_dig = backpacks.on_dig,
+		allow_metadata_inventory_put = backpacks.allow_metadata_inventory_put,
+	})
+
+	minetest.register_craft({
+		output = "backpacks:backpack_wool_" .. v[1],
+		recipe = {
+			{"wool:" .. v[1], "wool:" .. v[1], "wool:" .. v[1]},
+			{"wool:" .. v[1], "", "wool:" .. v[1]},
+			{"wool:" .. v[1], "wool:".. v[1], "wool:" .. v[1]},
+		}
+	})
+end
+
+if minetest.get_modpath("mobs") then
+	minetest.register_node("backpacks:backpack_leather", {
+		description = "Backpack (Leather)",
+		tiles = {
+			"backpacks_backpack_leather.png", -- Top
+			"backpacks_backpack_leather.png", -- Bottom
+			"backpacks_backpack_leather.png", -- Right Side
+			"backpacks_backpack_leather.png", -- Left Side
+			"backpacks_backpack_leather.png", -- Back
+			"backpacks_backpack_leather.png", -- Front
+		},
+		drawtype = "nodebox",
+		paramtype = "light",
+		paramtype2 = "facedir",
+		node_box = {
+			type = "fixed",
+			fixed = {
+				{-0.4375, -0.5, -0.375, 0.4375, 0.5, 0.375},
+				{0.125, -0.375, 0.4375, 0.375, 0.3125, 0.5},
+				{-0.375, -0.375, 0.4375, -0.125, 0.3125, 0.5},
+				{0.125, 0.1875, 0.375, 0.375, 0.375, 0.4375},
+				{-0.375, 0.1875, 0.375, -0.125, 0.375, 0.4375},
+				{0.125, -0.375, 0.375, 0.375, -0.25, 0.4375},
+				{-0.375, -0.375, 0.375, -0.125, -0.25, 0.4375},
+				{-0.3125, -0.375, -0.4375, 0.3125, 0.1875, -0.375},
+				{-0.25, -0.3125, -0.5, 0.25, 0.125, -0.4375},
+			}
+		},
+		groups = {dig_immediate = 3, oddly_diggable_by_hand = 3},
+		stack_max = 1,
+		on_construct = backpacks.on_construct,
+		after_place_node = backpacks.after_place_node,
+		on_dig = backpacks.on_dig,
+		allow_metadata_inventory_put = backpacks.allow_metadata_inventory_put,
+	})
+
+	minetest.register_craft({
+		output = "backpacks:backpack_leather",
+		recipe = {
+			{"mobs:leather", "mobs:leather", "mobs:leather"},
+			{"mobs:leather", "", "mobs:leather"},
+			{"mobs:leather", "mobs:leather", "mobs:leather"},
+		}
+	})
+end
